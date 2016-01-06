1ba8d7cb4bf017a6da2854f96c476a8da7f8949c
John James Jacoby <johnjamesjacoby@me.com>
2016-01-06 09:07:52 -0600

    Update cap checks to include have/edit/view options.

--- includes/capabilities.php
+++ includes/capabilities.php
@@ -21,21 +21,46 @@ defined( 'ABSPATH' ) || exit;
  */
 function wp_user_parents_map_meta_cap( $caps = array(), $cap = '', $user_id = 0, $args = array() ) {
 
-	// Bail if not checking `edit_user` cap
-	if ( 'edit_user' !== $cap ) {
-		return  $caps;
-	}
+	// Copy the caps array so a pristene one can be passed below
+	$new_caps = $caps;
 
-	// Bail if user is checking themselves
-	if ( $user_id === $args[0] ) {
-		return $caps;
-	}
+	// Modify core caps, and check for newly mapped ones
+	switch ( $cap ) {
 
-	// Grant cap if parent
-	if ( wp_is_user_parent_of_user( $args[0], $user_id ) ) {
-		$caps = array( 'read' );
+		// Parent management
+		case 'edit_user' :
+
+			// Break if editing self
+			if ( $user_id === $args[0] ) {
+				break;
+			}
+
+			// Grant cap if parent
+			if ( wp_is_user_parent_of_user( $args[0], $user_id ) ) {
+				$new_caps = array( 'read' );
+			}
+
+			break;
+
+		// Have
+		case 'have_user_parents'  :
+		case 'have_user_children' :
+			$new_caps = array( 'exist' );
+			break;
+
+		// View
+		case 'view_user_parents'  :
+		case 'view_user_children' :
+			$new_caps = array( 'exist' );
+			break;
+
+		// Edit
+		case 'edit_user_parents'  :
+		case 'edit_user_children' :
+			$new_caps = array( $cap );
+			break;
 	}
 
 	// Return possibly modified capabilities array
-	return $caps;
+	return apply_filters( 'wp_user_parents_map_meta_cap', $new_caps, $caps, $cap, $user_id, $args );
 }
